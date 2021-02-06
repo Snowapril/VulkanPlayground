8703665801d12629a46a809d34947913d6dc09fa
snowapril <sinjihng@gmail.com>
2021-02-06 12:56:14 +0900

    feat : replace initial sources with vkguide tutorial code

---
+++ Sources/main.cpp
@@ -0,0 +1,15 @@
+#include <vk_engine.h>
+
+int main(int argc, char* argv[])
+{
+	(void)argc; (void)argv;
+	VulkanEngine engine;
+
+	engine.init();	
+	
+	engine.run();	
+
+	engine.cleanup();	
+
+	return 0;
+}
